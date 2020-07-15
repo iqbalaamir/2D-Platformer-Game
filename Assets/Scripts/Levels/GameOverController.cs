2ee4129e07b754c43bfb661cc8691cbc63fd5e22
aamir <aamir.iqbal040@gmail.com>
2020-07-16 00:56:28 +0530

    player health added

---
+++ Assets/Scripts/Levels/GameOverController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverController : MonoBehaviour
+{
+   public string Scene;
+   public Button buttonRestart;
+   
+
+    private void Awake()
+    {
+        gameObject.SetActive(false);
+        buttonRestart.onClick.AddListener(ReloadScene);
+    }
+
+    public void PlayerDied()
+    {
+
+        gameObject.SetActive(true);
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(Scene);
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb2d;
 
     public ScoreController scoreController;
+    public GameOverController gameOverController;
+    public PlayerHealth playerHealth;
     public Animator animator;
     public BoxCollider2D boxcollider;
     public float speed;
@@ -23,27 +25,12 @@ public class PlayerController : MonoBehaviour
    
 
     private bool isOnGround;
-   
-
-    
-    public int score;
-    public GameObject gameOverText, restartButton;
-
-    public void KillPlayer()
-    {
-        animator.SetBool("Dead", true);
-        Debug.Log("Player Died By Enemy");
-    }
-     
-
-    public void PickUpKey()
-    {
-        Debug.Log("Picked Up Key");
-        scoreController.AddScore(score);
-    }
 
     private bool jumpInitaited;
-    
+    private Vector2 spawnPosition;
+
+    public int score;
+    private Vector3 respawn;
 
     private void Awake()
     {
@@ -54,12 +41,7 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    private void Start()
-    {
-        gameOverText.SetActive(false);
-        restartButton.SetActive(false);
-    }
-
+   
 
     // Update is called once per frame   
     private void Update()
@@ -155,5 +137,27 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    public void KillPlayer()
+    {
+            
+        if (playerHealth.health != 0)
+        {
+            playerHealth.ReduceHealth();
+            transform.position = respawn;
+        }
+        else
+        {
+            //animator.SetBool("Dead", true);
+            Debug.Log("Player Died By Enemy");
+            gameOverController.PlayerDied();
+        }
+    }
+
+
+    public void PickUpKey()
+    {
+        Debug.Log("Picked Up Key");
+        scoreController.AddScore(score);
+    }
 
 }
